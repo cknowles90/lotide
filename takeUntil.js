1a7ff62b31b45d4a5b12ae1aad26d7d733efcda3
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-21 17:58:44 -0700

    using a function to loop through an array and collect the keys, until a callback function retruns a truthy value, then test it with the assertArraysEqual function

---
+++ takeUntil.js
@@ -0,0 +1,46 @@
+const assertArraysEqual = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (j = i; j <= i; j++) {
+      if (arr1[i] !== arr2[j]) {
+        return console.log(`🔴 Assertion Failed: ${arr1} !== ${arr2}`)
+      }
+    }
+  }
+
+ return console.log(`🟢 Assertion Passed: ${arr1} === ${arr2}`)
+};
+
+
+const takeUntil = function(array, callback) {
+  const result = [];
+  
+  for (const item of array) {
+    if (!callback(item)) {
+      result.push(item);
+    } else {
+      break;
+    }
+  }
+  
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
+/* 
+
+[ 1, 2, 5, 7, 2 ]
+--
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+*/
