99fcc85a9514fd6fcbb98b73d1a8c713372dee82
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-25 14:59:02 -0700

    refactoring middle, and creating the middleTest file

--- middle.js
+++ middle.js
@@ -1,30 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-const assertArraysEqual = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    for (j = i; j <= i; j++) {
-      if (arr1[i] !== arr2[j]) {
-        return console.log(`🔴 Assertion Failed: ${arr1} !== ${arr2}`)
-      }
-    }
-  }
-
- return console.log(`🟢 Assertion Passed: ${arr1} === ${arr2}`)
-};
-
-
 const middle = function(array) {
   const newArray = [];
   const lengthOfArray = array.length;
@@ -42,12 +15,6 @@ const middle = function(array) {
   return newArray;
 };
 
+module.exports = middle;
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
