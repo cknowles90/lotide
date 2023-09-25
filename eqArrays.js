5fa498349ceb53999f04d15cd0a86a92d9e533b8
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-25 13:45:33 -0700

    module exports and require added to eqArrays, and the newly created eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,15 +10,6 @@ const eqArrays = function(arr1, arr2) {
   }
 
   return true;
-}
+};
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertEqual("Lighthouse Labs", 2);
-assertEqual(1, 2);
+module.exports = eqArrays;
