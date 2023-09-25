545d54dcd8ef4ad732be7295ed32a8da44b3d7aa
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-09-25 13:36:25 -0700

    continuing to update the lotide files with the new module calls, and creating test files for them - this commit is for tail.js & tailTest.js

--- tail.js
+++ tail.js
@@ -1,25 +1,7 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
+const tail = function(array) {
+  return array.slice(1);
 };
 
 
-const tail = function(array) {
-  return array.slice(1);
-}
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-
-assertEqual("Lighthouse Labs", 2);
-assertEqual(1, 2);
+module.exports = tail;
